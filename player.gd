186ef0af8213c4d861ef88e729d622f0fe8182d0
Rey S <276412reso@hcisd.org>
2025-09-27 17:17:23 -0500

    code

--- player.gd
+++ player.gd
@@ -13,14 +13,16 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
+	
+	if not Input.is_anything_pressed() or Input.is_action_just_pressed("jump"):
+		$Movement.frame = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
-		
+		$Movement.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
