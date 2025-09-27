40c348aa7c8329b30aeb71a9ff4051200775a42e
Rey S <276412reso@hcisd.org>
2025-09-27 16:27:59 -0500

    dirt

--- player.gd
+++ player.gd
@@ -14,10 +14,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	if not Input.is_anything_pressed():
-		$idle.visible = true
-		$movement.visible = false
-		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
